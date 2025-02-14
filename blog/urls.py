1e028cbbb25fb9ca42607a65a232fbefdd60f7f2
xcheche <chekwubechukwu23@gmail.com>
2025-02-14 21:33:03 +0100

    pagination

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from . import views
-from .views import (PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView)
+from .views import (PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, UserPostListView)
 
 
 urlpatterns = [
@@ -9,6 +9,7 @@ urlpatterns = [
     path("post/<int:pk>/update/", views.PostUpdateView.as_view(), name="post-update"),
     path("post/<int:pk>/delete/", views.PostDeleteView.as_view(), name="post-delete"),
     path("", PostListView.as_view(), name="home"), # PostListView.as_view() is a class-based view
+    path('user/<str:username>', UserPostListView.as_view(), name='user-posts'), # Post list view for a specific user
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'), # PostDetailView.as_view() is a class-based view
     path("about/", views.about, name="about"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import ( ListView, DetailView, CreateView, UpdateView, DeleteView )
 from .models import Post
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin , UserPassesTestMixin
+from django.contrib.auth.models import User
+
 
 # Create your views here.
 
@@ -12,7 +14,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin , UserPassesTestMixin
     
 
 #     return render(request, "blog/index.html", {"posts": posts})
-
+# Post list view
 class PostListView(ListView):
     model = Post
     template_name = 'blog/index.html'
@@ -20,9 +22,23 @@ class PostListView(ListView):
     ordering = ['-date_posted']
     paginate_by = 3
 
+
+#All posts by a specific user
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'blog/user_posts.html'  # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    paginate_by = 3
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
+
+
+
+
+
 # Detail page
-
-
 class PostDetailView(DetailView):
     model = Post
     template_name = 'blog/post_detail.html'  # uses object name by default
